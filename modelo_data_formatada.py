94d9a0b060013318817c01f251450fd0f9239417
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-07-23 15:15:07 -0300

    Rename several files and adding new file "modelo_TV.py" beyond new classes in package "minhas_classes"

--- modelo_data_formatada.py
+++ modelo_data_formatada.py
@@ -1,4 +1,4 @@
-from minhasClasses import Data
+from minhas_classes import Data
 from datetime import datetime
 d = Data(datetime.today().day, datetime.today().month, datetime.today().year)
 print(d.data_formatada())
